aa1bcf03e59db31da410209226186f8679996271
Hyeseung Ryu <hyeseungryu@Hyeseungui-MacBook-Pro.local>
2020-12-20 15:17:25 +0900

    add packages and go.mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ryulitaro/greetings
+
+go 1.15
---
+++ multi/multi.go
@@ -0,0 +1,45 @@
+package multi
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// Hellos returns a map that associates each of the named people
+// with a greeting message.
+func Hellos(names []string) (map[string]string, error) {
+	// A map to associate names with messages.
+	messages := make(map[string]string)
+	// Loop through the received slice of names, calling
+	// the Hello function to get a message for each name.
+	for _, name := range names {
+		if name == "" {
+			return nil, errors.New("empty name")
+		}
+		// In the map, associate the retrieved message with
+		// the name.
+		messages[name] = fmt.Sprintf(randomFormat(), name)
+	}
+	return messages, nil
+}
+
+// Init sets initial values for variables used in the function.
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	// Return one of the message formats selected at random.
+	return formats[rand.Intn(len(formats))]
+}
